c7c66486bf336e93a4e2b0459aa7d8f399d47cb6
Muffin <marvin.dick96@web.de>
2018-06-13 19:50:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="footer">
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+}
+</script>
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="container-fluid bg-4 text-center" id="myFooter">
+    <p>Example Footer made by Marvin Dick</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #myFooter {
+    padding-top: 70px;
+    padding-bottom: 70px;
+  }
+
+  .bg-4 {
+    background-color: #2f2f2f;
+    color: #ffffff;
+  }
+</style>
